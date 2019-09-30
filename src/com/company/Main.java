1f236e1d8fcf9dfe62fc96cfa351e9bf47dc6ef7
hamed.rajabi@itdcanada.ca <hamed.rajabi@itdcanada.ca>
2019-09-30 10:38:49 -0700

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+	// store this "world" in a variable
+        //type of variable = "World"
+        String anyName = "World";
+        System.out.println("Hello "+anyName);
+        System.out.print("I love java");
+    }
+}
